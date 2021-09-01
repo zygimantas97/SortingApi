45765d3467802fd138df91ea668b49105fc76348
zygimantas97 <zygimantas1997@gmail.com>
2021-09-01 23:39:25 +0300

    Added Performance Tests

--- SortingApi.PerformanceTests/Program.cs
+++ SortingApi.PerformanceTests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace SortingApi.PerformanceTests
@@ -8,12 +9,35 @@ namespace SortingApi.PerformanceTests
     {
         static void Main(string[] args)
         {
-            var sequence = new List<int> {3,5,1,4,2};
-            var sortedSequence = Utilities.Sorting.MergeSort(sequence);
-            foreach(var item in sortedSequence)
+            var countsOfNumbers = new List<int> { 10, 100, 1000, 10000, 100000 };
+            var random = new Random();
+            var stopwatch = new Stopwatch();
+
+            foreach (var count in countsOfNumbers)
             {
-                Console.WriteLine(item);
+                var sequence = new List<int>();
+
+                for(int i = 0; i < count; i++)
+                {
+                    sequence.Add(random.Next(1, count + 1));
+                }
+
+                Console.WriteLine(new String('-', 30));
+                Console.WriteLine(String.Format("{0, -20} {1}", "Count of elements:", count));
+
+                stopwatch.Start();
+                Utilities.Sorting.MergeSort(sequence);
+                stopwatch.Stop();
+                Console.WriteLine(String.Format("{0, -20} {1} ms", "MergeSort:", stopwatch.ElapsedMilliseconds));
+
+                stopwatch.Restart();
+                Utilities.Sorting.BubbleSort(sequence);
+                stopwatch.Stop();
+                Console.WriteLine(String.Format("{0, -20} {1} ms", "BubbleSort:", stopwatch.ElapsedMilliseconds));
+
+                stopwatch.Reset();
             }
+            Console.WriteLine(new String('-', 30));
         }
     }
 }
