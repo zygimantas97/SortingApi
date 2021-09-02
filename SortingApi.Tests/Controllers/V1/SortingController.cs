1b8fccf4528f439263a07caba3a013d26673dd56
zygimantas97 <zygimantas1997@gmail.com>
2021-09-02 14:20:23 +0300

    Added tests

---
+++ SortingApi.Tests/Controllers/V1/SortingController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SortingApi.Contracts.V1.Requests;
+using SortingApi.Contracts.V1.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingApi.Tests.Controllers.V1
+{
+    [TestFixture]
+    public class SortingController
+    {
+        [Test]
+        public void SortSequence_WithCorrectFormatOfSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sortingRequest = new SortingRequest
+            {
+                Sequence = "3 5 1 4 2",
+                Algorithm = AlgorithmType.BubbleSort
+            };
+            var expectedSortedSequence = "1 2 3 4 5";
+            var sortingController = new SortingApi.Controllers.V1.SortingController();
+
+            // Act
+            var actionResult = sortingController.SortSequence(sortingRequest);
+            var objectResult = actionResult as ObjectResult;
+            sortingController.DeleteLastSortedSequence();
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.AreEqual(201, objectResult.StatusCode);
+            Assert.IsInstanceOf<SortingResponse>(objectResult.Value);
+            Assert.AreEqual(expectedSortedSequence, (objectResult.Value as SortingResponse).SortedSequence);
+        }
+
+        [Test]
+        public void SortSequence_WithIncorrectFormatOfSequence_ReturnsBadRequest()
+        {
+            // Arrange
+            var sortingRequest = new SortingRequest
+            {
+                Sequence = "three five one four two",
+                Algorithm = AlgorithmType.BubbleSort
+            };
+            var sortingController = new SortingApi.Controllers.V1.SortingController();
+
+            // Act
+            var actionResult = sortingController.SortSequence(sortingRequest);
+            var objectResult = actionResult as ObjectResult;
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.AreEqual(400, objectResult.StatusCode);
+        }
+
+        [Test]
+        public void GetLastSortedSequence_WithExistingSortedSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sortingRequest = new SortingRequest
+            {
+                Sequence = "3 5 1 4 2",
+                Algorithm = AlgorithmType.BubbleSort
+            };
+            var expectedSortedSequence = "1 2 3 4 5";
+            var sortingController = new SortingApi.Controllers.V1.SortingController();
+
+            // Act
+            sortingController.SortSequence(sortingRequest);
+            var actionResult = sortingController.GetLastSortedSequence();
+            var objectResult = actionResult as ObjectResult;
+            sortingController.DeleteLastSortedSequence();
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.AreEqual(200, objectResult.StatusCode);
+            Assert.IsInstanceOf<SortingResponse>(objectResult.Value);
+            Assert.AreEqual(expectedSortedSequence, (objectResult.Value as SortingResponse).SortedSequence);
+        }
+
+        [Test]
+        public void GetLastSortedSequence_WithNotExistingSortedSequence_ReturnsNotFound()
+        {
+            // Arrange
+            var sortingController = new SortingApi.Controllers.V1.SortingController();
+
+            // Act
+            var actionResult = sortingController.GetLastSortedSequence();
+            var objectResult = actionResult as ObjectResult;
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.AreEqual(404, objectResult.StatusCode);
+        }
+
+        [Test]
+        public void DeleteLastSortedSequence_WithExistingSortedSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sortingRequest = new SortingRequest
+            {
+                Sequence = "3 5 1 4 2",
+                Algorithm = AlgorithmType.BubbleSort
+            };
+            var expectedSortedSequence = "1 2 3 4 5";
+            var sortingController = new SortingApi.Controllers.V1.SortingController();
+
+            // Act
+            sortingController.SortSequence(sortingRequest);
+            var actionResult = sortingController.DeleteLastSortedSequence();
+            var objectResult = actionResult as ObjectResult;
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.AreEqual(200, objectResult.StatusCode);
+            Assert.IsInstanceOf<SortingResponse>(objectResult.Value);
+            Assert.AreEqual(expectedSortedSequence, (objectResult.Value as SortingResponse).SortedSequence);
+        }
+
+        [Test]
+        public void DeleteLastSortedSequence_WithNotExistingSortedSequence_ReturnsNotFound()
+        {
+            // Arrange
+            var sortingController = new SortingApi.Controllers.V1.SortingController();
+
+            // Act
+            var actionResult = sortingController.DeleteLastSortedSequence();
+            var objectResult = actionResult as ObjectResult;
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.AreEqual(404, objectResult.StatusCode);
+        }
+    }
+}
---
+++ SortingApi.Tests/Utilities/Sorting.cs
@@ -0,0 +1,199 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingApi.Tests.Utilities
+{
+    [TestFixture]
+    public class Sorting
+    {
+        [Test]
+        public void BubbleSort_WithSortedSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                1, 2, 3, 4, 5
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.BubbleSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void BubbleSort_WithReversedSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                5, 4, 3, 2, 1
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.BubbleSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void BubbleSort_WithRandomSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                3, 5, 1, 4, 2
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.BubbleSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void BubbleSort_WithSequenceOfOneElement_ReturnsSequenceOfOneElement()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                1
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.BubbleSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void BubbleSort_WithEmptySequence_ReturnsEmptySequence()
+        {
+            // Arrange
+            var sequence = new List<int>();
+            var expectedSortedSequence = new List<int>();
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.BubbleSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void MergeSort_WithSortedSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                1, 2, 3, 4, 5
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.MergeSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void MergeSort_WithReversedSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                5, 4, 3, 2, 1
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.MergeSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void MergeSort_WithRandomSequence_ReturnsSortedSequence()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                3, 5, 1, 4, 2
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.MergeSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void MergeSort_WithSequenceOfOneElement_ReturnsSequenceOfOneElement()
+        {
+            // Arrange
+            var sequence = new List<int>
+            {
+                1
+            };
+            var expectedSortedSequence = new List<int>()
+            {
+                1
+            };
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.MergeSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+
+        [Test]
+        public void MergeSort_WithEmptySequence_ReturnsEmptySequence()
+        {
+            // Arrange
+            var sequence = new List<int>();
+            var expectedSortedSequence = new List<int>();
+
+            // Act
+            var actualSortedSequence = SortingApi.Utilities.Sorting.MergeSort(sequence);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedSortedSequence, actualSortedSequence);
+        }
+    }
+}
